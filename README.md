bd8ab65815f8f80cd2557f274ecade57de7aeedf
deepu <deepuchembra@gmail.com>
2020-11-05 10:13:08 +0530

    Iris eda app using streamlit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # iris_eda_streamlit
 Iris eda app using streamlit
+##Required Files
+setup.sh
+Procfile
+requirements.txt
+
---
+++ iris_eda.py
@@ -0,0 +1,8 @@
+import streamlit as st
+def main():
+    ##simmple demo
+    st.title("EDA app using Streamlit")
+    st.subheader("Welcome to ICFOSS")
+if __name__ == '__main__':
+    main()
+    
